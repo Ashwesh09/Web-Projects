9c3c587a946c5ca0f7cbae9e73bc0180c4cc672a
Ashwesh09 <ashwesh.chn@gmail.com>
2022-03-29 08:48:37 +0530

    new Commits

--- HelloSpring/src/com/springdemo/CStartedEventHandler.java
+++ HelloSpring/src/com/springdemo/CStartedEventHandler.java
@@ -4,6 +4,7 @@ import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextStartedEvent;
 
 public class CStartedEventHandler implements ApplicationListener<ContextStartedEvent> {
+	//Test comment
 	public void onApplicationEvent(ContextStartedEvent event) {
 		System.out.println("ContextStartedEvent handled");
 	}
